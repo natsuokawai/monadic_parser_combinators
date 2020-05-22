957c2611a6c3a5c71591646cb73caf1e2bec5763
natsuokawai <kawai.natsuo@gmail.com>
2020-05-22 20:55:47 +0900

    Alternative

--- parser.hs
+++ parser.hs
@@ -38,3 +38,12 @@ instance Monad Parser where
                          []        -> []
                          [(v,out)] -> parse (f v) out)
 
+instance Alternative Parser where
+  -- empty :: Parser a
+  empty = P (\inp -> [])
+
+  --- (<|>) :: Parser a -> Parser a -> Parser a
+  p <|> q = P (\inp -> case parse p inp of
+                         []        -> parse q inp
+                         [(v,out)] -> [(v,out)])
+
